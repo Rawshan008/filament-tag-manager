f0bcf0cb8b4200f014caa0a12c371094c925e034
rawshankcg <pust.love008@gmail.com>
2025-06-29 17:00:18 +0600

    Update Category

---
+++ readme.md
@@ -0,0 +1,1 @@
+### composer require rawshan008/filament-tag-manager
--- src/Resources/CategoryResource.php
+++ src/Resources/CategoryResource.php
@@ -18,6 +18,8 @@ class CategoryResource extends Resource
     protected static ?string $model = Category::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationGroup = 'Content Management';
+    protected static ?int $navigationSort = 1;
 
     public static function form(Form $form): Form
     {
